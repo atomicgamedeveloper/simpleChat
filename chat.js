4bd790f8e4d88b35dcd047fdd73d6b69a8dc15bb
au667066 <enokmalik@gmail.com>
2023-11-05 02:00:49 +0100

    Streaming added

--- chat.js
+++ chat.js
@@ -1,15 +1,23 @@
-var chatHistory = document.getElementById('chatHistory');
-var inputBox = document.getElementById('inputBox');
-var newButton = document.getElementById('newButton');
-const toggleSwitch = document.getElementById("toggleSwitch");
-toggleSwitch.checked = true;
 const fs = require('fs');
+const OpenAI = require('openai');
 let openAIKey = ""
 try {
     openAIKey = fs.readFileSync('openai.txt', 'utf8')
 } catch (err) {
     console.error('Error reading file:', err);
 }
+const openai = new OpenAI({
+    apiKey: openAIKey,
+    dangerouslyAllowBrowser: true,
+
+})
+
+var chatHistory = document.getElementById('chatHistory');
+var inputBox = document.getElementById('inputBox');
+var newButton = document.getElementById('newButton');
+const toggleSwitch = document.getElementById("toggleSwitch");
+toggleSwitch.checked = true;
+
 
 let model = "gpt-3.5-turbo";
 if (toggleSwitch.checked) {
@@ -18,25 +26,6 @@ if (toggleSwitch.checked) {
     model = "gpt-3.5-turbo";
 }
 let messages = [];
-
-async function fetchGPTStream(messages, model) {
-    try {
-        const stream = await axios.post('https://api.openai.com/v1/chat/completions', {
-            'model': model,
-            'messages': messages,
-            'temperature': 1,
-            'stream': true,
-        }, {
-            headers: {
-                'Authorization': `Bearer ${openAIKey}`,
-                'Content-Type': 'application/json',
-            }
-        });
-        return stream;
-    } catch (error) {
-        console.error(error);
-    }
-};
 
 async function fetchGPTResponse(messages, model) {
     try {
@@ -77,12 +66,17 @@ inputBox.addEventListener('keydown', async function (e) {
             messages.push({ "role": "user", "content": userMessage });
             inputBox.value = '';
             chatHistory.scrollTop = chatHistory.scrollHeight;
-            const botStream = fetchGPTStream(messages, model);
+            const stream = await openai.chat.completions.create({
+                'model': model,
+                'messages': messages,
+                'stream': true,
+            });
             const oldHistory = chatHistory.innerHTML;
             let addedHistory = "";
-            for await (const part of botStream) {
-                addedHistory += part;
+            for await (const part of stream) {
+                addedHistory += part.choices[0]?.delta?.content || '';
                 chatHistory.innerHTML = oldHistory + `<p>${model.toUpperCase()}: ${marked.parse(addedHistory)}</p>`;
+                chatHistory.scrollTop = chatHistory.scrollHeight;
             }
             chatHistory.scrollTop = chatHistory.scrollHeight;
             messages.push({ "role": "assistant", "content": addedHistory });
